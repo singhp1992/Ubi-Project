f94235525931e686faac418a1209599ecd6dedfd
Preeti <singhp1992@gmail.com>
2019-07-26 14:48:31 +0200

    creating student controller public class

--- src/main/java/com/project/controller/StudentController.java
+++ src/main/java/com/project/controller/StudentController.java
@@ -1,4 +1,15 @@
 package com.project.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class StudentController {
+    @RequestMapping("/students")
+    @ResponseBody
+    public String listStudents() {
+        return "List of all Students!";
+
+    }
 }
