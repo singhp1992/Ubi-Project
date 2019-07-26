161d1a4bf528651408309f5538d388785d1d097e
Preeti <singhp1992@gmail.com>
2019-07-26 15:24:44 +0200

    removed responsebody from student controller

--- src/main/java/com/project/controller/StudentController.java
+++ src/main/java/com/project/controller/StudentController.java
@@ -2,19 +2,16 @@ package com.project.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class StudentController {
     @RequestMapping("/")
-    @ResponseBody
     public String home() {
         return "Home Page";
 
     }
 
     @RequestMapping("/students")
-    @ResponseBody
     public String listStudents() {
         return "List of all Students!";
 
